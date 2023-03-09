feab5aeb8151e1ecf596395b3e2a8dc4d3738b0e
chung hee su <68191058+heesu-chung@users.noreply.github.com>
2023-03-09 15:44:27 +0900

    pr - test non button

--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -6,7 +6,18 @@ import { ARScreen } from './xr/arScreen'
 export function Scene() {
     return (
         <>
-            <ARButton />
+            {/* <ARButton
+                sessionInit={{
+                    optionalFeatures: [
+                        'local-floor',
+                        'bounded-floor',
+                        'hand-tracking',
+                        'layers',
+                    ],
+                }}
+                enterOnly={true}
+                exitOnly={true}
+            /> */}
             <Canvas>
                 <ARScreen />
             </Canvas>
