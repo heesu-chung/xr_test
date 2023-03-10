9d3bff177e4b8a9892273d01dcdcfdf3c8ee91be
chung hee su <68191058+heesu-chung@users.noreply.github.com>
2023-03-10 11:04:51 +0900

    new pr

--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -1,9 +1,10 @@
 import { Box, OrbitControls } from '@react-three/drei'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber'
 import { ARButton, useXR, XR } from '@react-three/xr'
 import { useRef } from 'react'
-
 import { ARScreen } from './xr/arScreen'
+
+import { USDZExporter } from 'three-stdlib'
 
 export function Scene() {
     return (
@@ -33,9 +34,24 @@ export function Scene() {
                     border: '2px solid #ccc',
                 }}
             />
-            <Canvas>
+            <a href="/usdz/Smart_Watch_KW_19.usdz" rel="ar" target="_blank">
+                USDZ
+            </a>
+            <img src="/usdz/Smart_Watch_KW_19.usdz" alt="" />
+            {/* <Canvas>
                 <ARScreen />
-            </Canvas>
+            </Canvas> */}
         </>
     )
 }
+
+// function USDZModel () {
+//     const { scene } = useThree();
+//     const exporter = new USDZExporter();
+//     exporter.parse(scene, function(usedz) {
+//         const blob = new Blob([usdz], { type: 'model/vnd.usdz+zip'});
+//         const link = document.createElement('a');
+//         link.href = URL.createObjectURL(blob);
+//         link.download
+//     })
+// }
